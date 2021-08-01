c3d4cabc0522f6c0d257d8c4f44904362f10b425
Rafael Padron <rafael.padron@gmail.com>
2021-07-31 21:05:19 -0400

    feat: implementa modula data

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { CoreModule } from '@nx-ionic/core';
 import { CourseModule } from '@nx-ionic/course';
 
 @Module({
-  imports: [CoreModule, CourseModule,DataModule],
+  imports: [CoreModule, CourseModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/data/src/lib/data.service.ts
+++ libs/data/src/lib/data.service.ts
@@ -2,22 +2,20 @@ import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/commo
 import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 
-
 @Injectable()
 export class DataService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-
-  private readonly defaultAdmin: { email: string; password: string }
+  private readonly defaultAdmin: { email: string; password: string };
 
   constructor(private readonly config: ConfigService) {
     super();
-    this.defaultAdmin = this.config.get('admin')
+    this.defaultAdmin = this.config.get('admin');
   }
   async onModuleDestroy() {
     await this.$disconnect();
   }
   async onModuleInit() {
     await this.$connect();
-    await this.ensureAdminUser()
+    await this.ensureAdminUser();
   }
   async createUser({ email, password }: { email: string; password: string }) {
     return this.user.create({
@@ -25,8 +23,7 @@ export class DataService extends PrismaClient implements OnModuleInit, OnModuleD
         email,
         password,
       },
-    })
-
+    });
   }
 
   async findUserByEmail(email: string) {
@@ -34,7 +31,7 @@ export class DataService extends PrismaClient implements OnModuleInit, OnModuleD
       where: {
         email,
       },
-    })
+    });
   }
 
   async findUserById(userId: number) {
@@ -42,21 +39,21 @@ export class DataService extends PrismaClient implements OnModuleInit, OnModuleD
       where: {
         id: userId,
       },
-    })
+    });
   }
 
   private async ensureAdminUser() {
     // Check if we have an admin
-    const found = await this.findUserByEmail(this.defaultAdmin.email)
+    const found = await this.findUserByEmail(this.defaultAdmin.email);
 
     if (found) {
-      Logger.log(`Admin user: ${found.email}`, 'DataService')
-      return true
+      Logger.log(`Admin user: ${found.email}`, 'DataService');
+      return true;
     }
 
     // If not, create it for us
-    const created = await this.createUser(this.defaultAdmin)
+    const created = await this.createUser(this.defaultAdmin);
 
-    Logger.log(`Created Admin user: ${created.email}`, 'DataService')
+    Logger.log(`Created Admin user: ${created.email}`, 'DataService');
   }
 }
